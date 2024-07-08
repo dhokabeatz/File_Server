8f94102b38a6b61c05fc5b0402d33280b852de27
dhokabeatz <amglna2020@gmail.com>
2024-07-08 07:33:54 -0400

    Deployment script added

--- FileServer/FileServer/settings.py
+++ FileServer/FileServer/settings.py
@@ -20,16 +20,13 @@ LOGOUT_REDIRECT_URL = 'landing_page'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-q*e!8%q2cd$x=rk=t4!=fc1m=w%zwv1-0%gb*=kd!mk18ix=9("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
