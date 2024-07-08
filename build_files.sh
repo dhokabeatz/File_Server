4b3436d2a606c61e56931b54e399a484b249b5fa
dhokabeatz <amglna2020@gmail.com>
2024-07-08 12:05:49 -0400

    Build file updated

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,4 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python manage.py collectstatic --noinput
