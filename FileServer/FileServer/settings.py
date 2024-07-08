313239ab54a72eda26d5d2a30b57a2ef4023f1e1
dhokabeatz <amglna2020@gmail.com>
2024-07-08 07:43:04 -0400

    Static file defined

--- FileServer/FileServer/settings.py
+++ FileServer/FileServer/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -144,7 +146,6 @@ DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "backend/static",
 ]
